8aa194b11182ed947ef1a4610b9d8b0ec5b400f9
antklim <antklim@gmail.com>
2024-03-07 16:41:02 +1100

    refactor: file updater

---
+++ src/update/DepsVersionTransformStream.ts
@@ -0,0 +1,36 @@
+import { Diff } from "../diff/mod.ts";
+
+export default class DepsVersionTransformStream
+  extends TransformStream<string, string> {
+  #currentLine = 0;
+  #diffs: Diff[] = [];
+  #diffIdx = 0;
+
+  constructor(diffs: Diff[]) {
+    super({
+      transform: (chunk, controller) => {
+        ++this.#currentLine;
+
+        if (this.#diffIdx < 0) {
+          controller.enqueue(chunk);
+          return;
+        }
+
+        let c = chunk;
+        const diff = this.#diffs[this.#diffIdx];
+
+        if (this.#currentLine === diff.referenceLine) {
+          c = chunk.replace(`@${diff.oldVersion}`, `@${diff.newVersion}`);
+
+          if (++this.#diffIdx >= this.#diffs.length) {
+            this.#diffIdx = -1;
+          }
+        }
+
+        controller.enqueue(c);
+      },
+    });
+
+    this.#diffs = diffs;
+  }
+}
--- src/update/updateFile.ts
+++ src/update/updateFile.ts
@@ -1,39 +1,6 @@
 import { type Diff } from "../diff/mod.ts";
 import { streams } from "deps";
-
-class DepsVersionTransformStream extends TransformStream<string, string> {
-  #currentLine = 0;
-  #diffs: Diff[] = [];
-  #diffIdx = 0;
-
-  constructor(diffs: Diff[]) {
-    super({
-      transform: (chunk, controller) => {
-        ++this.#currentLine;
-
-        if (this.#diffIdx < 0) {
-          controller.enqueue(chunk);
-          return;
-        }
-
-        let c = chunk;
-        const diff = this.#diffs[this.#diffIdx];
-
-        if (this.#currentLine === diff.referenceLine) {
-          c = chunk.replace(`@${diff.oldVersion}`, `@${diff.newVersion}`);
-
-          if (++this.#diffIdx >= this.#diffs.length) {
-            this.#diffIdx = -1;
-          }
-        }
-
-        controller.enqueue(c);
-      },
-    });
-
-    this.#diffs = diffs;
-  }
-}
+import DepsVersionTransformStream from "./DepsVersionTransformStream.ts";
 
 export interface UpdateFileProps {
   file: string;
